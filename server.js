81ce17edb9f6f453bad4ab79f8901b8c3e98a5ac
nirmal <nirmalw978@gmail.com>
2024-05-21 06:59:46 +0530

    3

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const path = require('path')
 
 app.use('/', express.static(path.join(__dirname,'/public')))
 app.use('/', require('./routes/root'))
-
+app.all('*',(req,res)={
+  
+})
 
 app.listen(PORT,()=> console.log(`server running on port ${PORT}`))
