9175625b549b0b5bac6bcd57f73e53a696702aba
nirmal <nirmalw978@gmail.com>
2024-05-21 22:27:02 +0530

    4

--- server.js
+++ server.js
@@ -6,8 +6,17 @@ const path = require('path')
 
 app.use('/', express.static(path.join(__dirname,'/public')))
 app.use('/', require('./routes/root'))
-app.all('*',(req,res)={
-  
+app.all('*',(req,res)=>{
+  res.status(404)
+  if(req.accepts('html')){
+    res.sendFile(path.join(__dirname,'view','404.html'))
+
+  }else if(req.accepts('json')){
+    res.json({messege : '404 not found'})
+  }else{
+    res.type('txt').send('404 not found')
+  }
+
 })
 
 app.listen(PORT,()=> console.log(`server running on port ${PORT}`))
